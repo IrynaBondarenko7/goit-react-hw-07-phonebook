44e841826bc95ec700534d3892efd403acd366e4
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-05-31 18:18:16 +0100

    added start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { ContactList } from './Contacts/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { StyledLayout } from './Layout/Layout.styled';
+import {
+  StyledPhonebookWrap,
+  StyledContactsTitle,
+  StyledTitle,
+  StyledTitleWrap,
+} from './App.styled';
+import { FaBook } from 'react-icons/fa';
+import { IoMdContacts } from 'react-icons/io';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <StyledLayout>
+      <StyledPhonebookWrap>
+        <StyledTitleWrap>
+          <StyledTitle>Phonebook</StyledTitle>
+          <FaBook />
+        </StyledTitleWrap>
+
+        <ContactForm />
+        <StyledTitleWrap>
+          <StyledContactsTitle>Contacts</StyledContactsTitle>
+          <IoMdContacts />
+        </StyledTitleWrap>
+
+        <Filter />
+        <ContactList />
+      </StyledPhonebookWrap>
+    </StyledLayout>
   );
 };
